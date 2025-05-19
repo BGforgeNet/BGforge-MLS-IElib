80b289625a6cb68fb07179c4ea35294ef94781ee
Magus <magus@bgforge.net>
2025-05-19 19:50:13 +0700

    ts: add slots ids

--- ts/ielib/bg2/slots.ids.ts
+++ ts/ielib/bg2/slots.ids.ts
@@ -1,2 +1,86 @@
 /** Slots.ids */
 export declare type Slots = number & {};
+/** 0 */
+export declare const SLOT_AMULET: Slots;
+/** 1 */
+export declare const SLOT_ARMOR: Slots;
+/** 2 */
+export declare const SLOT_BELT: Slots;
+/** 3 */
+export declare const SLOT_BOOTS: Slots;
+/** 4 */
+export declare const SLOT_CLOAK: Slots;
+/** 5 */
+export declare const SLOT_GAUNTLETS: Slots;
+/** 6 */
+export declare const SLOT_HELMET: Slots;
+/** 7 */
+export declare const SLOT_RING_LEFT: Slots;
+/** 8 */
+export declare const SLOT_RING_RIGHT: Slots;
+/** 9 */
+export declare const SLOT_SHIELD: Slots;
+/** 10 */
+export declare const SLOT_FIST: Slots;
+/** 11 */
+export declare const SLOT_AMMO: Slots;
+/** 15 */
+export declare const SLOT_MISC: Slots;
+/** 35 */
+export declare const SLOT_WEAPON: Slots;
+/** 11 */
+export declare const SLOT_AMMO0: Slots;
+/** 12 */
+export declare const SLOT_AMMO1: Slots;
+/** 13 */
+export declare const SLOT_AMMO2: Slots;
+/** 14 */
+export declare const SLOT_AMMO3: Slots;
+/** 15 */
+export declare const SLOT_MISC0: Slots;
+/** 16 */
+export declare const SLOT_MISC1: Slots;
+/** 17 */
+export declare const SLOT_MISC2: Slots;
+/** 18 */
+export declare const SLOT_MISC3: Slots;
+/** 19 */
+export declare const SLOT_MISC4: Slots;
+/** 20 */
+export declare const SLOT_MISC5: Slots;
+/** 21 */
+export declare const SLOT_MISC6: Slots;
+/** 22 */
+export declare const SLOT_MISC7: Slots;
+/** 23 */
+export declare const SLOT_MISC8: Slots;
+/** 24 */
+export declare const SLOT_MISC9: Slots;
+/** 25 */
+export declare const SLOT_MISC10: Slots;
+/** 26 */
+export declare const SLOT_MISC11: Slots;
+/** 27 */
+export declare const SLOT_MISC12: Slots;
+/** 28 */
+export declare const SLOT_MISC13: Slots;
+/** 29 */
+export declare const SLOT_MISC14: Slots;
+/** 30 */
+export declare const SLOT_MISC15: Slots;
+/** 31 */
+export declare const SLOT_MISC16: Slots;
+/** 32 */
+export declare const SLOT_MISC17: Slots;
+/** 33 */
+export declare const SLOT_MISC18: Slots;
+/** 34 */
+export declare const SLOT_MISC19: Slots;
+/** 35 */
+export declare const SLOT_WEAPON0: Slots;
+/** 36 */
+export declare const SLOT_WEAPON1: Slots;
+/** 37 */
+export declare const SLOT_WEAPON2: Slots;
+/** 38 */
+export declare const SLOT_WEAPON3: Slots;
